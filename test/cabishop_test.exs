f4b7862394f7e1ba1e9ebaf1d14d7c222ab2e80b
Oscar Javier Moreno Rey <ojmorenor@gmail.com>
2017-08-12 21:55:27 -0500

    Add tests for checkout process taking into account discounts

--- test/cabishop_test.exs
+++ test/cabishop_test.exs
@@ -2,7 +2,62 @@ defmodule CabiShopTest do
   use ExUnit.Case
   doctest CabiShop
 
-  test "greets the world" do
-    assert CabiShop.hello() == :world
+  alias CabiShop.{Checkout, Factory, Product}
+
+  alias :mnesia, as: Mnesia
+
+  describe "checkout process" do
+    setup do
+      Factory.build_voucher()
+      |> Product.create()
+
+      Factory.build_mug()
+      |> Product.create()
+
+      Factory.build_tshirt()
+      |> Product.create()
+
+      {:ok, order} = Checkout.start()
+
+      on_exit fn ->
+        Mnesia.clear_table(Products)
+      end
+
+      {:ok, %{order: order}}
+    end
+
+    test "success simple checkout process", %{order: order} do
+      Checkout.scan(order, "VOUCHER")
+      Checkout.scan(order, "TSHIRT")
+      Checkout.scan(order, "MUG")
+      assert Checkout.total(order) == 32.50
+    end
+
+    test "success checkout with voucher discount", %{order: order} do
+      Checkout.scan(order, "VOUCHER")
+      Checkout.scan(order, "TSHIRT")
+      Checkout.scan(order, "VOUCHER")
+      assert Checkout.total(order) == 25.00
+    end
+
+    test "success checkout with tshirt discount", %{order: order} do
+      Checkout.scan(order, "TSHIRT")
+      Checkout.scan(order, "TSHIRT")
+      Checkout.scan(order, "TSHIRT")
+      Checkout.scan(order, "VOUCHER")
+      Checkout.scan(order, "TSHIRT")
+      assert Checkout.total(order) == 81.00
+    end
+
+    test "success tshirt and voucher discount", %{order: order} do
+      Checkout.scan(order, "VOUCHER")
+      Checkout.scan(order, "TSHIRT")
+      Checkout.scan(order, "VOUCHER")
+      Checkout.scan(order, "VOUCHER")
+      Checkout.scan(order, "MUG")
+      Checkout.scan(order, "TSHIRT")
+      Checkout.scan(order, "TSHIRT")
+      assert Checkout.total(order) == 74.50
+    end
   end
 end
