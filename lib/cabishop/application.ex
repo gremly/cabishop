3afefa8d1b7bb4e8cb9b6c5750b2261550be5667
Oscar Javier Moreno Rey <ojmorenor@gmail.com>
2017-08-12 21:56:18 -0500

    Start checkout supervisor from the main application module

--- lib/cabishop/application.ex
+++ lib/cabishop/application.ex
@@ -12,14 +12,10 @@ defmodule CabiShop.Application do
     # Ensure to initialize the database
     Repo.init()
 
-    # List all child processes to be supervised
     children = [
-      # Starts a worker by calling: CabiShop.Worker.start_link(arg)
-      # {CabiShop.Worker, arg},
+      {CabiShop.CheckoutSupervisor, type: :supervisor}
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: CabiShop.Supervisor]
     Supervisor.start_link(children, opts)
   end
