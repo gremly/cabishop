fa5d3115dea247beb202111de8421c2d6d3cfa50
Oscar Javier Moreno Rey <ojmorenor@gmail.com>
2017-08-13 14:01:02 -0500

    Add main API to make easy the usage of the application

--- lib/cabishop.ex
+++ lib/cabishop.ex
@@ -1,18 +1,48 @@
 defmodule CabiShop do
   @moduledoc """
-  Documentation for CabiShop.
+  This module is the main entry point to use
+  the CabiShop API.
+
+  Here are exposed functions to manage checkout process
+  and products in a consistent way.
   """
+
+  alias CabiShop.{Checkout, Product}
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> CabiShop.hello
-      :world
-
+  Returns the total price of the current checkout process with all
+  promotions and discounts applied for each product.
   """
-  def hello do
-    :world
+  defdelegate total(checkout), to: Checkout
+
+  @doc "Starts a new checkout process"
+  def start_checkout do
+    Checkout.start()
+  end
+
+  @doc "Add products to the current checkout process"
+  def scan_product(checkout, product) do
+    Checkout.scan(checkout, product)
+  end
+
+  @doc "Stops the checkout process"
+  def finish_checkout(checkout) do
+    Checkout.finish(checkout)
+  end
+
+  @doc """
+  Creates a new product.
+
+  This function recieves as parameter a Product structure which is defined
+  as follows:
+
+  %Product{code: "code", name: "name", price: 1.00, rules: %{rule: %{val: a...}}}
+  """
+  def new_product(%Product{} = product) do
+    Product.create(product)
+  end
+
+  def get_product(code) do
+    Product.get(code)
   end
 end
